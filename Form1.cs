ae5779963b1ddd9c84cd87da0730423890dcc9fd
MuhammadChoirulRizal <mchoirulrizal9@gmail.com>
2026-03-10 13:06:54 +0800

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace AkademikADOApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Status(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void DIsconnect(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Connect(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
